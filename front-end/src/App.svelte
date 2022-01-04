d6ca76a186490aeab587f2f7996933356c6fbb0b
Gustavo Farias <gustavocel82@gmail.com>
2022-01-04 14:31:27 -0300

    start-front

---
+++ front-end/src/App.svelte
@@ -0,0 +1,50 @@
+<script>
+	import ClientChat from "./components/ClientChat.svelte";
+	import ServerChat from "./components/ServerChat.svelte";
+</script>
+
+<h1>Chat app!</h1>
+<div id='chatWrapper'>
+	<div class="chatInnerWrapper">
+		<h2>Cliente</h2>
+		<ClientChat class="chatWindow" />
+	</div>
+	<div class="chatInnerWrapper">
+		<h2>Servidor</h2>
+		<ServerChat class="chatWindow"/>
+	</div>	
+</div>
+
+
+<style>
+
+	h1,h2 {
+		color: white;
+		text-align: center;
+	}
+	#chatWrapper {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+		height: 100%;
+		gap: 15px;
+	}
+
+	.chatInnerWrapper {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		width: 100%;
+		height: 100%;
+	}
+
+	:global(.chatWindow) {
+		height: 50%;
+		width: 70%;
+		background-color: rgb(163, 255, 186);
+		box-shadow: 0 2px 7px 0 rgba(0,0,0,0.2);
+		position: relative;
+		padding: 0;
+	}
+</style>
---
+++ front-end/src/components/ClientChat.svelte
@@ -0,0 +1,44 @@
+<script>
+  function submit(e) {
+    e.preventDefault()
+    console.log('submit');
+  }
+</script>
+
+
+<div class={$$props.class}>
+  <div>
+    <form on:submit={submit}>
+      <input type="text" placeholder="Digite sua mensagem..." />
+      <button type="submit">Enviar</button>
+    </form>
+    
+  </div>
+</div>
+
+<style>
+  input {
+    width: 100%;
+    height: 50px;
+    position: absolute;
+    padding-left: 15px;
+    bottom: 0;
+    margin: 0;
+    color: black;
+  }
+  button {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    height: 50px;
+    padding:0;
+    margin: 0;
+    width: 80px;
+    border: none;
+    background-color: rgb(31, 144, 69);
+    color: white;
+  }
+</style>
+
+
+  
